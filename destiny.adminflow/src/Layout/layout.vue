825c4dbf95addbe348d2650733c26e1d6b28fd9b
wangzewei <wzw0126>
2020-04-09 19:04:06 +0800

    Ts摸索已完成，引入IviewUI组件

---
+++ destiny.adminflow/src/Layout/layout.vue
@@ -0,0 +1,11 @@
+<template>
+    <Layout>
+        <Sider hide-trigger>Sider</Sider>
+        <Layout>
+            <Header>Header</Header>
+            <Content>Content</Content>
+            <Footer>Footer</Footer>
+        </Layout>
+    </Layout>
+</template>
+<script lang="ts" src="./layout.ts"></script>
