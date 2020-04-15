8228d540f9d4a5cc308691ea60fe1e5c3eeddc3e
wangzewei <wzw0126>
2020-04-15 10:08:43 +0800

    修复Layout中Tag标签点击关闭不生效

--- destiny.adminflow/src/Layout/layout.vue
+++ destiny.adminflow/src/Layout/layout.vue
@@ -45,12 +45,12 @@
                 <div class="scroll-body">
                   <span v-for=" item in this.$store.state.TagList" :key="item.path">
                     <span v-if="item.IsColse==true">
-                      <Tag type="dot" closable v-if="item.path==$route.path" on-close="Coldses" color="primary"><router-link :to="item.path">{{item.lable}}</router-link> </Tag>
-                      <Tag type="dot" closable v-else > <router-link :to="item.path">{{item.lable}}</router-link ></Tag>
+                      <Tag type="dot" closable v-if="item.path==$route.path" @on-close="Coldses" :name="item.path" color="primary"><router-link :to="item.path">{{item.lable}}</router-link> </Tag>
+                      <Tag type="dot" closable v-else :name="item.path"  > <router-link :to="item.path">{{item.lable}}</router-link ></Tag>
                     </span>
                     <span v-else>
-                      <Tag type="dot" v-if="item.path==$route.path" color="primary"><router-link :to="item.path">{{item.lable}}</router-link></Tag>
-                      <Tag type="dot" v-else ><router-link :to="item.path"> {{item.lable}}</router-link></Tag>
+                      <Tag type="dot" v-if="item.path==$route.path" :name="item.path"  color="primary"><router-link :to="item.path">{{item.lable}}</router-link></Tag>
+                      <Tag type="dot" v-else :name="item.path"  ><router-link :to="item.path"> {{item.lable}}</router-link></Tag>
                     </span>
                   </span>
                 </div>
