c93692d7408f71838379479ae94ef5926a460e9d
卢汝东 <lurudonga@outlook.com>
2020-04-17 22:19:15 +0800

    修改用户列表

--- destiny.adminflow/src/views/user-view/user.vue
+++ destiny.adminflow/src/views/user-view/user.vue
@@ -1,11 +1,69 @@
 
 <template>
-  <div>
+  <Card :bordered="false">
+    <div class="content">
+      <Form ref="formInline" inline>
+        <FormItem prop="user">
+          <Input type="text" placeholder="Username" />
+          <Icon type="ios-person-outline" slot="prepend"></Icon>
+        </FormItem>
+      </Form>
+    </div>
+    <i-table :columns="columns" :data="TableData"></i-table>
+    <Page :total="Total"></Page>
+  </Card>
+  <!-- <div>
     <i-table :columns="columns" :data="TableData"></i-table>
     <div>
       <Page :total="Total"></Page>
     </div>
-  </div>
+  </div>-->
 </template>
 <script lang="ts" src="./user.ts">
 </script>
+<style lang="less" >
+.table-page-search-wrapper {
+  // .ivu-form-inline {
+  //   .ivu-form-item {
+  //     display: flex;
+  //     margin-bottom: 24px;
+  //     margin-right: 0;
+
+  //     .ivu-input-wrapper {
+  //       flex: 1 1;
+  //       display: inline-block;
+  //       vertical-align: middle;
+  //     }
+
+  //     > .ivu-form-item-label {
+  //       line-height: 32px;
+  //       padding-right: 8px;
+  //       width: auto;
+  //     }
+  //     .ivu-form-item-content {
+  //       height: 32px;
+  //       line-height: 32px;
+  //     }
+  //   }
+  // }
+  .table-page-search-submitButtons {
+    display: block;
+    margin-bottom: 24px;
+    white-space: nowrap;
+  }
+}
+
+.table {
+  height: 100%;
+}
+.content {
+  .table-operator {
+    margin-bottom: 18px;
+
+    button {
+      margin-right: 8px;
+    }
+  }
+}
+</style>>
+
