5c902cea7de3f98db741b1671fcadcfedda742e8
So魚丸丷 <dygood@outlook.com>
2020-05-29 16:10:46 +0800

    全面删除sass,将启动命令替换成start

--- destiny.adminflow/src/components/menu/MenuTab.vue
+++ destiny.adminflow/src/components/menu/MenuTab.vue
@@ -1,10 +1,13 @@
 <template>
-
-<div>
+  <div>
     <Menu @on-select="Openmenu" :active-name="$route.path" :theme="theme2">
-      <MenuRecursion :key="item.name" v-for="item in MenuArr" :item="item"></MenuRecursion>
+      <MenuRecursion
+        :key="item.name"
+        v-for="item in MenuArr"
+        :item="item"
+      ></MenuRecursion>
     </Menu>
   </div>
 </template>
 <script lang="ts" src="./menu.ts"></script>
-<style lang="scss" src="./menu.scss"></style>
+<style lang="less" src="./menu.less"></style>
